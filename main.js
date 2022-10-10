d485cfd0f3ce2dc7efff67ce7393df614fd25230
Moomen Eltelbany <moomenmohamed4@gmail.com>
2022-10-10 17:50:08 -0400

    Setting up some defaults
    Removing the default active class on the difficulty

--- main.js
+++ main.js
@@ -21,220 +21,211 @@ let languageSelect;
 
 // Adding the active class to the chosen category
 languagesBtns.forEach((btn) => {
-    btn.onclick = () => {
-        languagesBtns.forEach((btn) => {
-            btn.classList.remove("active");
-        });
-        btn.classList.add("active");
-        categoryName.innerHTML = btn.innerHTML;
-    };
+  btn.onclick = () => {
+    languagesBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+    categoryName.innerHTML = btn.innerHTML;
+  };
 });
 
 // Adding the active class to the chosen timer
 timerBtns.forEach((btn) => {
-    btn.onclick = () => {
-        timerSpan.innerHTML = btn.dataset.timer;
-        timerBtns.forEach((btn) => {
-            btn.classList.remove("active");
-        });
-        btn.classList.add("active");
-    };
+  btn.onclick = () => {
+    timerSpan.innerHTML = btn.dataset.timer;
+    timerBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+  };
 });
 // Starting the game by clicking on the start button
 startGame.onclick = () => {
-    // Remove the button from the page, so that we can't click on it again
-    startGame.style.display = "none";
+  // Remove the button from the page, so that we can't click on it again
+  startGame.style.display = "none";
 
-    // Here we pass the parameter to choose which category we will generate the questions
-    getsQuestion(categoryName.innerHTML.toLowerCase());
+  // Here we pass the parameter to choose which category we will generate the questions
+  getsQuestion(categoryName.innerHTML.toLowerCase());
 };
 
 // The function that will generate the questions and answers
 function getsQuestion(fileName) {
-    let myRequest = new XMLHttpRequest();
+  let myRequest = new XMLHttpRequest();
 
-    myRequest.onreadystatechange = () => {
-        if (myRequest.readyState === 4 && myRequest.status === 200) {
-            let questionObject = JSON.parse(myRequest.responseText);
-            let questionObjectLength = questionObject.length;
-            // Create the question count number and the span bullets
-            spanBullets(questionObject);
+  myRequest.onreadystatechange = () => {
+    if (myRequest.readyState === 4 && myRequest.status === 200) {
+      let questionObject = JSON.parse(myRequest.responseText);
+      let questionObjectLength = questionObject.length;
+      // Create the question count number and the span bullets
+      spanBullets(questionObject);
 
-            // Setting the total Question numbers
-            totalAnswers.innerHTML = questionObjectLength;
+      // Setting the total Question numbers
+      totalAnswers.innerHTML = questionObjectLength;
 
-            // Get the questions and answers
-            showQuestions(questionObject[indexCount], questionObjectLength);
+      // Get the questions and answers
+      showQuestions(questionObject[indexCount], questionObjectLength);
 
-            // Initialize the correct answer count
-            rightAnswers.innerHTML = rightAnswersCount;
+      // Initialize the correct answer count
+      rightAnswers.innerHTML = rightAnswersCount;
 
-            // Click on the submit button function
-            timer(+timerSpan.innerHTML, questionObjectLength);
-            submitBtn.onclick = () => {
-                // Check if the question are finished or not
-                // If yes, the button will be removed as well as the answers
-                if (indexCount === questionObjectLength - 1) {
-                    // Remove the submit button
-                    submitBtn.remove();
-                    // After one second, we remove the whole quiz area (Question and answers)
-                    setTimeout(() => {
-                        quizAnswers.innerHTML = "";
-                    }, 1000);
-                    // We check the result only if the last questions
-                    checkTheRightAnswer(
-                        questionObject[questionObjectLength - 1]["right_answer"]
-                    );
-                    if (+rightAnswers.innerHTML < +totalAnswers.innerHTML / 2) {
-                        result.classList.add("bad");
-                        result.innerHTML = "Bad Score,";
-                    } else if (
-                        +rightAnswers.innerHTML < +totalAnswers.innerHTML
-                    ) {
-                        result.classList.add("good");
-                        result.innerHTML = "Moderate Score,";
-                    } else {
-                        result.classList.add("perfect");
-                        result.innerHTML = "Perfect Score,";
-                    }
-                } else {
-                    let rightAnswer =
-                        questionObject[indexCount]["right_answer"];
+      // Click on the submit button function
+      timer(+timerSpan.innerHTML, questionObjectLength);
+      submitBtn.onclick = () => {
+        // Check if the question are finished or not
+        // If yes, the button will be removed as well as the answers
+        if (indexCount === questionObjectLength - 1) {
+          // Remove the submit button
+          submitBtn.remove();
+          // After one second, we remove the whole quiz area (Question and answers)
+          setTimeout(() => {
+            quizAnswers.innerHTML = "";
+          }, 1000);
+          // We check the result only if the last questions
+          checkTheRightAnswer(
+            questionObject[questionObjectLength - 1]["right_answer"]
+          );
+          if (+rightAnswers.innerHTML < +totalAnswers.innerHTML / 2) {
+            result.classList.add("bad");
+            result.innerHTML = "Bad Score,";
+          } else if (+rightAnswers.innerHTML < +totalAnswers.innerHTML) {
+            result.classList.add("good");
+            result.innerHTML = "Moderate Score,";
+          } else {
+            result.classList.add("perfect");
+            result.innerHTML = "Perfect Score,";
+          }
+        } else {
+          let rightAnswer = questionObject[indexCount]["right_answer"];
 
-                    // A function to check between the chosen answer and the right one
-                    checkTheRightAnswer(rightAnswer, questionObjectLength);
+          // A function to check between the chosen answer and the right one
+          checkTheRightAnswer(rightAnswer, questionObjectLength);
 
-                    //  Increment the index count by one
-                    indexCount++;
+          //  Increment the index count by one
+          indexCount++;
 
-                    // Empty the current question and get the next one
-                    quizAnswers.innerHTML = "";
+          // Empty the current question and get the next one
+          quizAnswers.innerHTML = "";
 
-                    // Get the next question
-                    showQuestions(
-                        questionObject[indexCount],
-                        questionObjectLength
-                    );
+          // Get the next question
+          showQuestions(questionObject[indexCount], questionObjectLength);
 
-                    nextSpanOn(indexCount);
+          nextSpanOn(indexCount);
 
-                    // The count down function
-                    clearInterval(countDownInterval);
-                    timer(+timerSpan.innerHTML, questionObjectLength);
-                }
-            };
+          // The count down function
+          clearInterval(countDownInterval);
+          timer(+timerSpan.innerHTML, questionObjectLength);
         }
-    };
-    myRequest.open("GET", `${fileName}.json`, true);
-    myRequest.send();
+      };
+    }
+  };
+  myRequest.open("GET", `${fileName}.json`, true);
+  myRequest.send();
 }
 
 // A function to create the question count number and the bullets spans
 function spanBullets(obj) {
-    questionCount.innerHTML = obj.length;
+  questionCount.innerHTML = obj.length;
 
-    for (let i = 0; i < obj.length; i++) {
-        let bulletSpan = document.createElement("span");
+  for (let i = 0; i < obj.length; i++) {
+    let bulletSpan = document.createElement("span");
 
-        if (i === 0) {
-            bulletSpan.className = "on";
-        }
-
-        spansArea.appendChild(bulletSpan);
+    if (i === 0) {
+      bulletSpan.className = "on";
     }
+
+    spansArea.appendChild(bulletSpan);
+  }
 }
 
 // Function for next span on
 function nextSpanOn(ind) {
-    let span = Array.from(document.querySelectorAll(".spans span"));
+  let span = Array.from(document.querySelectorAll(".spans span"));
 
-    span[ind].className = "on";
+  span[ind].className = "on";
 }
 
 // Function to get questions and put them inside the HTML page
 function showQuestions(obj) {
-    // Create the question P and appending it to the main div
-    let pQuestion = document.createElement("p");
-    pQuestion.className = "question";
+  // Create the question P and appending it to the main div
+  let pQuestion = document.createElement("p");
+  pQuestion.className = "question";
 
-    // As the question inside the object is with the name of title, we used it to add to the page
-    pQuestion.innerText = obj.title;
+  // As the question inside the object is with the name of title, we used it to add to the page
+  pQuestion.innerText = obj.title;
 
-    let arr = [1, 2, 3, 4];
-    let current = arr.length,
-        random;
+  // Starting shuffling the array so that we shuffle the answers every time we start the game
+  let arr = [1, 2, 3, 4];
+  let current = arr.length,
+    random;
 
-    // a loop to go through all the element inside the array but our last element
-    while (current > 0) {
-        // Here we get a random number that we will use it as a index to swap it with another number
-        random = Math.floor(Math.random() * current);
+  // a loop to go through all the element inside the array but our last element
+  while (current > 0) {
+    // Here we get a random number that we will use it as a index to swap it with another number
+    random = Math.floor(Math.random() * current);
 
-        current--;
+    current--;
 
-        // Here we use the destructuring method to sway the arr[current] which is each element from the last to the beginning with the arr[random] which is a random element from the array
-        [arr[current], arr[random]] = [arr[random], arr[current]];
-    }
+    // Here we use the destructuring method to sway the arr[current] which is each element from the last to the beginning with the arr[random] which is a random element from the array
+    [arr[current], arr[random]] = [arr[random], arr[current]];
+  }
 
-    console.log(arr);
-    // The for loop to create the divs that contains all the answers
-    for (let i = 1; i <= 4; i++) {
-        let answerDiv = document.createElement("div");
-        answerDiv.className = "answer";
+  console.log(arr);
+  // The for loop to create the divs that contains all the answers
+  for (let i = 0; i < 4; i++) {
+    let answerDiv = document.createElement("div");
+    answerDiv.className = "answer";
 
-        let radioBtn = document.createElement("input");
-        radioBtn.type = "radio";
-        radioBtn.name = "questions";
-        radioBtn.id = `answer_${arr[i]}`;
-        radioBtn.dataset.answer = obj[`answer_${arr[i]}`];
+    let radioBtn = document.createElement("input");
+    radioBtn.type = "radio";
+    radioBtn.name = "questions";
+    radioBtn.id = `answer_${arr[i]}`;
+    radioBtn.dataset.answer = obj[`answer_${arr[i]}`];
 
-        if (i === 1) {
-            radioBtn.checked = true;
-        }
+    let labelArea = document.createElement("label");
+    labelArea.setAttribute("for", `answer_${arr[i]}`);
+    labelArea.innerText = obj[`answer_${arr[i]}`];
 
-        let labelArea = document.createElement("label");
-        labelArea.setAttribute("for", `answer_${arr[i]}`);
-        labelArea.innerText = obj[`answer_${arr[i]}`];
-
-        answerDiv.appendChild(radioBtn);
-        answerDiv.appendChild(labelArea);
-        quizAnswers.appendChild(answerDiv);
-    }
-    quizAnswers.prepend(pQuestion);
+    answerDiv.appendChild(radioBtn);
+    answerDiv.appendChild(labelArea);
+    quizAnswers.appendChild(answerDiv);
+  }
+  quizAnswers.prepend(pQuestion);
 }
 
 // Function that takes the right answer and compare it to the chosen answer of the user
 function checkTheRightAnswer(rightAnswer) {
-    let allInputs = Array.from(document.querySelectorAll(".answer input"));
-    let chosenAnswer;
+  let allInputs = Array.from(document.querySelectorAll(".answer input"));
+  let chosenAnswer;
 
-    allInputs.forEach((answer) => {
-        if (answer.checked) {
-            chosenAnswer = answer.dataset.answer;
-        }
-    });
-
-    if (chosenAnswer === rightAnswer) {
-        // Increment the right answer by one
-        rightAnswersCount++;
-        rightAnswers.innerHTML = rightAnswersCount;
+  allInputs.forEach((answer) => {
+    if (answer.checked) {
+      chosenAnswer = answer.dataset.answer;
     }
+  });
+
+  if (chosenAnswer === rightAnswer) {
+    // Increment the right answer by one
+    rightAnswersCount++;
+    rightAnswers.innerHTML = rightAnswersCount;
+  }
 }
 
 // The count down function that takes the duration and make a count down
 function timer(duration) {
-    let minutes, seconds;
+  let minutes, seconds;
 
-    countDownInterval = setInterval(() => {
-        minutes = parseInt(duration / 60);
-        seconds = parseInt(duration % 60);
+  countDownInterval = setInterval(() => {
+    minutes = parseInt(duration / 60);
+    seconds = parseInt(duration % 60);
 
-        minutes = minutes < 10 ? `0${minutes}` : minutes;
-        seconds = seconds < 10 ? `0${seconds}` : seconds;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-        countDown.innerHTML = `${minutes} : ${seconds}`;
-        if (--duration < 0) {
-            clearInterval(countDownInterval);
-            submitBtn.click();
-        }
-    }, 1000);
+    countDown.innerHTML = `${minutes} : ${seconds}`;
+    if (--duration < 0) {
+      clearInterval(countDownInterval);
+      submitBtn.click();
+    }
+  }, 1000);
 }
